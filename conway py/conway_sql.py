dbe62a1a161803b988907afeb20d81710b97af2f
whock <whockei1@jhmi.edu>
2016-04-28 20:43:13 -0400

    further revisions. linked SQL to ecology.

--- conway py/conway_sql.py
+++ conway py/conway_sql.py
@@ -24,37 +24,45 @@ def create_db():
     cur.execute("CREATE TABLE Patterns(Id INT,Pattern TEXT)")
     cur.execute("CREATE TABLE Data(Id INT,PatFitness TEXT,PopFitness TEXT, Other TEXT)")
 
-
 def table_info(tname):
     '''get table metadata'''
     pass
-    
+
+
+def save_sentinel(sentinel):
+    data = sentinel['data']
+    metadata = sentinel['metadata']
+    patterns = sentinel['patterns']
+    for item in [data,metadata,patterns]:
+        _ = [package(item,i) for i in item]
+
 
 def get_unique_ID():
     return datetime.datetime.now().strftime("%y%m%d_%H%M")
- 
+
 
 def package(table,data):  #MAIN FUNCTION to insert data
     '''single field'''
     stringed = stringify(data)
     insert(table,stringed)
-    
-    
+
+
 def readout(table,uID): #MAIN FUNCTION to retrieve data
     data = query(table,uID)
     data = [destring(i,)]
-    
+
 
 def compress(data):
     '''remove all whitespace from a string'''
     return data.replace(" ","")
-   
+
 def insert(table,data):
-    uID = get_unique_ID()
+    uID = get_unique_ID() + data[0]
+    data = data[1:]
     data.insert(0,uID)
     cur.execute("INSERT INTO {} VALUES({})".format(table,data))
     con.commit()
-    
+
 def stringify(data):
     '''data is single field, eg metadata,pattern,fitness vec, etc. Assumed
     that elements of list / array are numeric'''
@@ -64,27 +72,27 @@ def stringify(data):
         _ = [[stringed.append(str(data[i,j])) for i in range(len(data))] for j in range(len(data[0]))]
     else:
         _ = [stringed.append(str(data[i])) for i in range(len(data))]
-    stringed = compress(stringed)    
+    stringed = compress(stringed)
     return stringed
 
 def reshape(data,rows,cols):
     '''take text data and reshape into matrix of floats, shape (rows,cols)'''
     data = [float(i) for i in data]
     return np.array(data).reshape(rows,cols)
-    
+
 def destring(data,ID,tname):
     if tname == 'Metadata':
         destringed = []
         meta = parse_meta([],ID)
         for k,v in meta.iteritems():
             destringed.append((k,v))
-            
+
     elif tname == 'Patterns':
         parsed = parse_meta(['w','h'],ID)
         w = parsed['w']
         h = parsed['h']
         destringed = reshape(data,h,w)
-        
+
     elif tname =='Data':
         data  = query('Data',ID)
         fit = data[1]
@@ -106,7 +114,7 @@ def query(tname,uID):
         return 'Entry {} in {} does not yet exist'.format(uID,tname)
     else:
         return found
-    
+
 def findsubstrings(data,targets):
     m = data.split(',')
     h ={}
@@ -120,15 +128,11 @@ def findsubstrings(data,targets):
     for i in hits:
         h[i[0].split(':')[0]] = float(i[0].split(':')[1])
     return h
- 
+
 def parse_meta(targets,uID):
     meta = query('Metadata',uID)
     hitdict = findsubstrings(meta[1],targets)
     return hitdict
-       
+
 def fin():
     con.close()
-
-    
-    
-    
